28b9b70fd0e96e582e59b9b03ea9c1150b559499
Brian Knorr <brianknorr@BRIANs-MBP.fios-router.home>
2016-03-16 19:57:00 -0500

    support nested dirs

--- index.js
+++ index.js
@@ -12,19 +12,34 @@ module.exports = function($) {
         };
 
     $.load = function() {
-        var process = function(name, list) {
+        var process = function(moduleName, list) {
+            var module = $[moduleName];
+
             _.each(list, function(item) {
-                $[name][s.camelize(item.name)] = item.module;
+                var splits = item.name.split('/');
+                if (splits.length > 1) {
+                    var ref = module;
+                    _.each(splits, function(split, index) {
+                        split = s.camelize(split);
+                        if (index === splits.length - 1) {
+                            ref[split] = item.module;
+                        } else {
+                            ref = ref[split] || (ref[split] = {});
+                        }
+                    });
+                } else {
+                    module[s.camelize(item.name)] = item.module;
+                }
             });
-            console.log('loaded', name, $[name]);
+            console.log('loaded', moduleName, module);
         };
 
-        process('lib', require('../../lib/*.js', {mode: 'list', options: { ignore: '../../lib/index.js'}}));
-        process('views', require('../../views/*.ejs', {mode: 'list', options: { ignore: '../../views/index.js'}}));
-        process('plugins', require('../../plugins/*.js', {mode: 'list', options: { ignore: '../../plugins/index.js'}}));
-        process('controllers', require('../../controllers/*.js', {mode: 'list', options: { ignore: '../../controllers/index.js'}}));
-        process('services', require('../../services/*.js', {mode: 'list', options: { ignore: '../../services/index.js'}}));
-        process('managers', require('../../managers/*.js', {mode: 'list', options: { ignore: '../../managers/index.js'}}));
+        process('lib', require('../../lib/**/*.js', {mode: 'list', options: { ignore: '../../lib/index.js'}}));
+        process('views', require('../../views/**/*.ejs', {mode: 'list', options: { ignore: '../../views/index.js'}}));
+        process('plugins', require('../../plugins/**/*.js', {mode: 'list', options: { ignore: '../../plugins/index.js'}}));
+        process('controllers', require('../../controllers/**/*.js', {mode: 'list', options: { ignore: '../../controllers/index.js'}}));
+        process('services', require('../../services/**/*.js', {mode: 'list', options: { ignore: '../../services/index.js'}}));
+        process('managers', require('../../managers/**/*.js', {mode: 'list', options: { ignore: '../../managers/index.js'}}));
 
         return $;
     };
