abf39c3d68f56b0492a6c365995af9328d411cd4
Brian Knorr <brianknorr@BRIANs-MBP-3.fios-router.home>
2016-10-19 19:48:33 -0500

    reverted to object based with index required last

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 var _ = require('underscore');
 var s = require('underscore.string');
 
-var $ = module.exports = {};
-
-var dirs = ['lib', 'views', 'plugins', 'controllers', 'services', 'managers', 'orchestrators'];
-_.each(dirs, function(dir) {
-    $[dir] = {};
-});
+var $ = module.exports = {
+    lib: {},
+    views: {},
+    plugins: {},
+    controllers: {},
+    services: {},
+    managers: {},
+    orchestrators: {}
+};
 
 $.load = function(_$) {
     if (_$) {
@@ -19,9 +22,14 @@ $.load = function(_$) {
     var process = function(moduleName, list) {
         var module = $[moduleName];
 
-        var indexItems = [];
+        _.each(list, function(item) {
+            //console.log('!!!item.name before', item.name);
+            item.name = item.name.split(moduleName+'/')[1];
 
-        var doItem = function(item) {
+            //console.log('!!!item.name', item.name);
+            if (item.name.indexOf('index') !== -1) {
+                return;
+            }
             var splits = item.name.split('/');
             if (splits.length > 1) {
                 var ref = module;
@@ -36,38 +44,30 @@ $.load = function(_$) {
             } else {
                 module[s.camelize(item.name)] = item.module;
             }
-        };
-
-        _.each(list, function(item) {
-            //console.log('!!!item.name before', item.name);
-            item.name = item.name.split(moduleName+'/')[1];
-
-            //console.log('!!!item.name', item.name);
-            if (item.name.indexOf('index') !== -1) {
-                return indexItems.push(item);
-            }
-            doItem(item);
-        });
-
-        _.each(indexItems, function(item) {
-            doItem(item);
         });
         console.log('loaded', moduleName, module);
     };
 
-    process('lib', require('../../lib/**/*.js', {mode: 'list', resolve:['path','strip-ext']}));
+    process('lib', require('../../lib/**/*.js', {mode: 'list', resolve:['path','strip-ext'], options: {ignore:'../../lib/**/index.js'} }));
+    require('../../lib/**/index.js', {mode: 'list'});
 
-    process('views', require('../../views/**/*{.ejs,.js}', {mode: 'list', resolve:['path','strip-ext']}));
+    process('views', require('../../views/**/*{.ejs,.js}', {mode: 'list', resolve:['path','strip-ext'], options: {ignore:'../../views/**/index.js'} }));
+    require('../../views/**/index.js', {mode: 'list'});
 
-    process('plugins', require('../../plugins/**/*.js', {mode: 'list', resolve:['path','strip-ext']}));
+    process('plugins', require('../../plugins/**/*.js', {mode: 'list', resolve:['path','strip-ext'], options: {ignore:'../../plugins/**/index.js'} }));
+    require('../../plugins/**/index.js', {mode: 'list'});
 
-    process('controllers', require('../../controllers/**/*.js', {mode: 'list', resolve:['path','strip-ext']}));
+    process('controllers', require('../../controllers/**/*.js', {mode: 'list', resolve:['path','strip-ext'], options: {ignore:'../../controllers/**/index.js'} }));
+    require('../../controllers/**/index.js', {mode: 'list'});
 
-    process('services', require('../../services/**/*.js', {mode: 'list', resolve:['path','strip-ext']}));
+    process('services', require('../../services/**/*.js', {mode: 'list', resolve:['path','strip-ext'], options: {ignore:'../../services/**/index.js'} }));
+    require('../../services/**/index.js', {mode: 'list'});
 
-    process('managers', require('../../managers/**/*.js', {mode: 'list', resolve:['path','strip-ext']}));
+    process('managers', require('../../managers/**/*.js', {mode: 'list', resolve:['path','strip-ext'], options: {ignore:'../../managers/**/index.js'} }));
+    require('../../managers/**/index.js', {mode: 'list'});
 
-    process('orchestrators', require('../../orchestrators/**/*.js', {mode: 'list', resolve:['path','strip-ext']}));
+    process('orchestrators', require('../../orchestrators/**/*.js', {mode: 'list', resolve:['path','strip-ext'], options: {ignore:'../../orchestrators/**/index.js'} }));
+    require('../../orchestrators/**/index.js', {mode: 'list'});
 
     return $;
 };
